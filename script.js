0e04ccd7394e37cd0ff64e012a63814d9e98c141
Roman <www.tolkoya@gmail.com>
2020-10-13 20:43:28 +0300

    feat(style.css): add styles for links and flags

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let countryes = [
   {
     label: 'Australia',
-    link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Flag_of_Australia_%28converted%29.svg/23px-Flag_of_Australia_%28converted%29.svg.png'
+    link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Flag_of_Australia.svg/800px-Flag_of_Australia.svg.png'
   },
   {
     label: 'Albania',
-    link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Flag_of_Albania.svg/21px-Flag_of_Albania.svg.png'
+    link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Flag_of_Albania.svg/800px-Flag_of_Albania.svg.png'
   },
 ];
 
@@ -15,6 +15,8 @@ let countryList = document.querySelector('.country-list'),
 let countryLi;
 let countryLink;
 let countryImage;
+let insideLiDiv;
+let linkDesc;
 
 let radioChange = document.querySelector('.onChange'),
     radioSubmit = document.querySelector('.onSubmit');
@@ -23,12 +25,18 @@ for (var i = 0; i < countryes.length; i++) {
   countryLi = document.createElement('li');
   countryLink = document.createElement('a');
   countryImage = document.createElement('img');
+  insideLiDiv = document.createElement('div');
+  linkDesc = document.createElement('a');
   countryLink.textContent = countryes[i].label;
   countryImage.src = countryes[i].link;
+  countryImage.classList.add('flag');
   countryLi.classList.add('country');
+  countryLink.classList.add('country-link');
   countryList.append(countryLi);
   countryLi.append(countryImage);
-  countryLi.append(countryLink);
+  countryLi.append(insideLiDiv);
+  insideLiDiv.append(countryLink);
+  insideLiDiv.append(linkDesc);
 }
 
 let list = Array.from(document.querySelectorAll('li'));
@@ -42,7 +50,7 @@ if (radioChange.checked) {
       if (!links[j].textContent.includes(filter)) {
         list[j].style.display = 'none';
       } else {
-        list[j].style.display = 'block';
+        list[j].style.display = 'flex';
       }
     }
   });
