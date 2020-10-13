1fe7720f8b57006e4067ad20bd632f954358a5cd
Roman <www.tolkoya@gmail.com>
2020-10-13 22:55:23 +0300

    feat: style radio-buttons, refactoring script.js, rewrite index.html

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let countryes = [
+let countries = [
   {
     label: 'Australia',
     link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Flag_of_Australia.svg/800px-Flag_of_Australia.svg.png'
@@ -18,64 +18,92 @@ let countryes = [
 ];
 
 let countryList = document.querySelector('.country-list'),
-    input = document.querySelector('.search');
+    searchInput = document.querySelector('.search');
 
-let countryLi;
-let countryLink;
-let countryImage;
-let insideLiDiv;
-let linkDesc;
+let countryLink,
+    countryTitle,
+    countryImage,
+    countryDesc,
+    countryLinkArrow,
+    insideLinkDiv;
+
 
 let radioChange = document.querySelector('.onChange'),
     radioSubmit = document.querySelector('.onSubmit');
 
-for (var i = 0; i < countryes.length; i++) {
-  countryLi = document.createElement('li');
+for (let i = 0; i < countries.length; i++) {
   countryLink = document.createElement('a');
+  countryTitle = document.createElement('p');
   countryImage = document.createElement('img');
-  insideLiDiv = document.createElement('div');
-  linkDesc = document.createElement('p');
-  countryLink.textContent = countryes[i].label;
-  countryImage.src = countryes[i].link;
-  countryImage.classList.add('flag');
-  countryLi.classList.add('country');
+  countryDesc = document.createElement('p');
+  insideLinkDiv = document.createElement('div');
+  countryLinkArrow = document.createElement('div');
+
+  countryTitle.textContent = countries[i].label;
+  countryDesc.textContent = `en.wikipedia.org/wiki/${countries[i].label}`;
+  countryLink.href = `https://${countryDesc.textContent}`;
+  countryImage.src = countries[i].link;
+  countryLinkArrow.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="10.806" height="17.5" viewBox="0 0 10.806 17.5">
+    <path id="ic_chevron_right_24px" d="M10.646,6,8.59,8.056l6.679,6.694L8.59,21.444,10.646,23.5l8.75-8.75Z" transform="translate(-8.59 -6)" fill="#333"/>
+    </svg>
+  `;
+
   countryLink.classList.add('country-link');
-  countryList.append(countryLi);
-  countryLi.append(countryImage);
-  countryLi.append(insideLiDiv);
-  insideLiDiv.append(countryLink);
-  insideLiDiv.append(linkDesc);
+  countryTitle.classList.add('country-title');
+  countryDesc.classList.add('country-desc');
+  countryImage.classList.add('flag');
+  countryLinkArrow.classList.add('country-link__arrow');
+
+  countryList.append(countryLink);
+  countryLink.append(countryImage);
+  countryLink.append(insideLinkDiv);
+  countryLink.append(countryLinkArrow);
+  insideLinkDiv.append(countryTitle);
+  insideLinkDiv.append(countryDesc);
 }
 
-let list = Array.from(document.querySelectorAll('li'));
-let links = Array.from(document.querySelectorAll('a'));
+let links = Array.from(document.querySelectorAll('.country-link')),
+    titles = Array.from(document.querySelectorAll('.country-title'));
 
-if (radioChange.checked) {
-  input.addEventListener('input', () => {
-    let filter = input.value.toLowerCase();
+searchInput.addEventListener('input', () => {
+  let filter = searchInput.value.toLowerCase();
 
-    for (var j = 0; j < list.length; j++) {
-      if (!links[j].textContent.toLowerCase().includes(filter)) {
-        list[j].style.display = 'none';
-      } else {
-        list[j].style.display = 'flex';
-      }
+  for (let j = 0; j < links.length; j++) {
+    if (!titles[j].textContent.toLowerCase().includes(filter)) {
+      links[j].style.display = 'none';
+    } else {
+      links[j].style.display = 'flex';
     }
-  });
-}
+  }
+});
 
-if (radioSubmit.checked) {
-  let searchButton = document.querySelector('.search-button');
-
-  searchButton.addEventListener('click', () => {
-    let filter = input.value.toLowerCase();
-
-    for (var j = 0; j < list.length; j++) {
-      if (!links[j].textContent.toLowerCase().includes(filter)) {
-        list[j].style.display = 'none';
-      } else {
-        list[j].style.display = 'flex';
-      }
-    }
-  });
-}
+// if (radioChange.checked) {
+//   searchInput.addEventListener('input', () => {
+//     let filter = searchInput.value.toLowerCase();
+//
+//     for (let j = 0; j < links.length; j++) {
+//       if (!titles[j].textContent.toLowerCase().includes(filter)) {
+//         links[j].style.display = 'none';
+//       } else {
+//         links[j].style.display = 'flex';
+//       }
+//     }
+//   });
+// }
+//
+// let searchButton = document.querySelector('.search-button');
+//
+// if (radioSubmit.checked) {
+//   searchButton.addEventListener('click', () => {
+//     let filter = searchInput.value.toLowerCase();
+//
+//     for (let j = 0; j < links.length; j++) {
+//       if (!titles[j].textContent.toLowerCase().includes(filter)) {
+//         links[j].style.display = 'none';
+//       } else {
+//         links[j].style.display = 'flex';
+//       }
+//     }
+//   });
+// }
