fa0870f8b0974c09feacb9cfaa456c1e4133cb3c
Roman <www.tolkoya@gmail.com>
2020-10-15 12:46:40 +0300

    feat(script.js): add and complite functional of sort and render countries

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ const countries = [
   {
     label: 'Japan',
     link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Japan.svg/800px-Flag_of_Japan.svg.png'
+  },
+  {
+    label: 'Bolivia',
+    link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Flag_of_Bolivia_%28state%29.svg/800px-Flag_of_Bolivia_%28state%29.svg.png'
   }
 ];
 
@@ -36,13 +40,14 @@ let radioChange = document.querySelector('#onChange'),
 
 // Перебор массива со странами с дальнейшей генерацией элементов html-страницы (списка стран)
 
-function sortCountries (array) {
-
-}
-
-let sortedCountries = [];
-
 function createCountriesInfo (label, link) {
+  countryLink = document.createElement('a');
+  countryTitle = document.createElement('p');
+  countryImage = document.createElement('img');
+  countryDesc = document.createElement('p');
+  insideLinkDiv = document.createElement('div');
+  countryLinkArrow = document.createElement('div');
+
   countryTitle.textContent = label;
   countryDesc.textContent = `en.wikipedia.org/wiki/${label}`;
   countryLink.href = `https://${countryDesc.textContent}`;
@@ -53,39 +58,48 @@ function createCountriesInfo (label, link) {
     <path id="ic_chevron_right_24px" d="M10.646,6,8.59,8.056l6.679,6.694L8.59,21.444,10.646,23.5l8.75-8.75Z" transform="translate(-8.59 -6)" fill="#333"/>
     </svg>
   `;
+
+  countryLink.classList.add('country-link');
+  countryTitle.classList.add('country-title');
+  countryDesc.classList.add('country-desc');
+  countryImage.classList.add('country-flag');
+  countryLinkArrow.classList.add('country-link__arrow');
+
+  countryList.append(countryLink);
+  countryLink.append(countryImage);
+  countryLink.append(insideLinkDiv);
+  countryLink.append(countryLinkArrow);
+  insideLinkDiv.append(countryTitle);
+  insideLinkDiv.append(countryDesc);
 }
 
+let links;
+
 function renderCountries (array) {
-  for (var i = 0; i < array.length; i++) {
-    countryLink = document.createElement('a');
-    countryTitle = document.createElement('p');
-    countryImage = document.createElement('img');
-    countryDesc = document.createElement('p');
-    insideLinkDiv = document.createElement('div');
-    countryLinkArrow = document.createElement('div');
-
-    createCountriesInfo (array[i].label, array[i].link);
-
-    countryLink.classList.add('country-link');
-    countryTitle.classList.add('country-title');
-    countryDesc.classList.add('country-desc');
-    countryImage.classList.add('country-flag');
-    countryLinkArrow.classList.add('country-link__arrow');
-
-    countryList.append(countryLink);
-    countryLink.append(countryImage);
-    countryLink.append(insideLinkDiv);
-    countryLink.append(countryLinkArrow);
-    insideLinkDiv.append(countryTitle);
-    insideLinkDiv.append(countryDesc);
+  if (array != countries) {
+    links.forEach((item) => {
+      item.remove();
+    });
   }
+
+  if (array == countries) {
+    for (let i = 0; i < array.length; i++) {
+      createCountriesInfo (array[i].label, array[i].link);
+    }
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < countries.length; j++) {
+      if (array[i] == countries[j].label) {
+        createCountriesInfo (countries[j].label, countries[j].link);
+      }
+    }
+  }
+
+  links = Array.from(document.querySelectorAll('.country-link'));
 }
 
 renderCountries (countries);
-
-// Создание массивов ссылок (стран) и названий стран
-let links = Array.from(document.querySelectorAll('.country-link')),
-    titles = Array.from(document.querySelectorAll('.country-title'));
 
 // Кнопка "Ок"
 const searchButton = document.querySelector('.search-button');
@@ -95,15 +109,15 @@ const searchButton = document.querySelector('.search-button');
 function renderCountriesList () {
   let regexp = new RegExp(`^${searchInput.value}`, `i`);
 
-  links.forEach((item) => {
-    item.remove();
-  });
+  let sortArray = [];
 
-  for (let j = 0; j < links.length; j++) {
+  for (let j = 0; j < countries.length; j++) {
     if (regexp.test(countries[j].label)) {
-      countryList.append(links[j]);
+      sortArray.push(countries[j].label);
     }
   }
+  sortArray.sort();
+  renderCountries(sortArray);
 }
 
 function handleOnChangePress () {
@@ -116,6 +130,7 @@ function handleOnChangePress () {
 
   searchInput.addEventListener('input', renderCountriesList);
   searchButton.removeEventListener('click', renderCountriesList);
+  searchButton.style.display = 'none';
 }
 
 function handleOnSubmitPress () {
@@ -126,6 +141,7 @@ function handleOnSubmitPress () {
   radioChange.removeAttribute('checked');
   radioSubmit.setAttribute('checked', 'checked');
 
+  searchButton.style.display = 'block';
   searchButton.addEventListener('click', renderCountriesList);
   searchInput.removeEventListener('input', renderCountriesList);
 }
@@ -133,9 +149,12 @@ function handleOnSubmitPress () {
 radioChange.addEventListener('click', handleOnChangePress);
 radioSubmit.addEventListener('click', handleOnSubmitPress);
 
+
+//переделать потом!
 if (radioChange.hasAttribute('checked')) {
   searchInput.addEventListener('input', renderCountriesList);
   searchButton.removeEventListener('click', renderCountriesList);
+  searchButton.style.display = 'none';
 } else {
   searchButton.addEventListener('click', renderCountriesList);
   searchInput.removeEventListener('input', renderCountriesList);
@@ -144,10 +163,10 @@ if (radioChange.hasAttribute('checked')) {
 // Проверка на checked и поиск из списка стран (ДОРАБОТАТЬ!)
 
 // Задачи:
-// 1. Написать функцию в которой мы создаем list-item по параметрам (label, link); +-
-// 2. Добавить сортировку по алфавитному порядку; ---
+// 1. Написать функцию в которой мы создаем list-item по параметрам (label, link); +
+// 2. Добавить сортировку по алфавитному порядку; +
 // 3. Чистить контейнер по изменению и отрисовывать элементы заного; +
-// 4. renderCoutries - принимает массив элементов и рендерит страны; +-
+// 4. renderCoutries - принимает массив элементов и рендерит страны; +
 // 5. Функцию управления handleOnChangePress и handleOnSubmitPress (удаление противоположного ивента) и проверка; +
 // 6. Чистые функции!!!
 // 7. Закончить с окном внизу "JSON"
